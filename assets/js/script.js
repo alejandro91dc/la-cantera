4a25fda0d6859955d352ff59e41a9c5423aac1c5
alejandro91dc <diazcarmona.alejandro91@gmail.com>
2023-12-23 21:48:37 +0100

    alternate-header

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    AOS.init();
+
+
+    var myvid = document.getElementById('myvideo');
+    var myvids = [
+        "http://www.w3schools.com/html/mov_bbb.mp4", 
+        "http://www.w3schools.com/html/movie.mp4"
+    ];
+    var activeVideo = 0;
+    
+    myvid.addEventListener('ended', function(e) {
+        if (activeVideo == 0){activeVideo = 1}
+        console.log("jiji");
+        myvid.src = myvids[activeVideo];
+        myvid.play();
+    });
+});
