c20edbddc3934267de99a8e9aced4c9437f8a7d0
alejandro91dc <diazcarmona.alejandro91@gmail.com>
2023-12-24 01:44:49 +0100

    boton esquivo pruebas

--- assets/js/anime-scripts.js
+++ assets/js/anime-scripts.js
@@ -4,6 +4,37 @@ import anime from '../animejs/lib/anime.es.js';
 $( document ).ready(function() {
 
 
+  const button = document.getElementById("runaway-btn");
+
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc"
+  });
+
+["mouseover", "click"].forEach(function (el) {
+  button.addEventListener(el, function (event) {
+    const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+    const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+
+    animateMove(this, "left", left).play();
+    animateMove(this, "top", top).play();
+  });
+});
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
+
+
+var boton = document.getElementById('runaway-btn');
+
+  // Agregar un evento de clic al botón
+  boton.addEventListener('click', function() {
+    // Mostrar un alert cuando se hace clic en el botón
+    alert('¡Que era broma cabezapolla!');
+  });
 
 /*
 anime({
