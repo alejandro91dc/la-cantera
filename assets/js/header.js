0521ff75e9c3cf0b00333cc3ced0963ef3cea200
alejandro91dc <diazcarmona.alejandro91@gmail.com>
2024-01-06 06:31:12 +0100

    text-box canteranos

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,16 +1,39 @@
-$(document).ready(function() {
-    var menuContainer = $('#headerContainer'); // Reemplaza con el selector correcto en tu página
+var currentPage;
+
+function loadHeader() {
+    var menuContainer = $('#headerContainer');
     var headerFileName = $('script[src$="assets/js/header.js"]').data('header');
-    
+
     $.ajax({
         url: headerFileName,
         method: 'GET',
         success: function(response) {
             menuContainer.html(response);
             menuContainer.css('display', 'block');
+
+            activateCurrentPageLink();
         },
         error: function() {
             console.error('Error al cargar el encabezado.');
         }
     });
+}
+function activateCurrentPageLink() {
+    currentPage = window.location.pathname.toLowerCase();  
+    console.log("Página actual:", currentPage);
+
+    $('#menu a').each(function () {
+        var linkPage = new URL(this.href).pathname.toLowerCase();  
+        console.log("Página del enlace:", linkPage);
+
+       
+        if (currentPage === linkPage) {
+            $(this).addClass('active');
+        }
+    });
+}
+
+$(document).ready(function() {
+    loadHeader();
+    activateCurrentPageLink();
 });
