50798a29eb3970f2ceb106ea614978692ceb14ba
alejandro91dc <diazcarmona.alejandro91@gmail.com>
2024-01-06 23:07:54 +0100

    pagina canteranos terminada a falta de contenido multimedia
    añadir video principal con enfoque en la zona derecha y oscuro a la izquierda para resaltar el texto y tres imagenes de tres personas que coincidan con la descripcion
    
    Co-Authored-By: MrJavato <145049881+MrJavato@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,19 @@
 $( document ).ready(function() {
     AOS.init();
 
-
 });
+    function toggleContent(boxId) {
+        // Oculta todas las cajas de contenido
+        var contentBoxes = document.getElementsByClassName('content-box');
+        for (var i = 0; i < contentBoxes.length; i++) {
+            contentBoxes[i].style.display = 'none';
+        }
+    
+        // Muestra la caja de contenido correspondiente al hacer clic
+        document.getElementById(boxId).style.display = 'block';
+    
+        // Si haces clic en una caja de contenido diferente a la introducción, oculta la introducción
+        if (boxId !== 'introduccion') {
+            document.getElementById('introduccion').style.display = 'none';
+        }
+    }
